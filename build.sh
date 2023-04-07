882f09a735e13f011adc388c0030b589a08fe227
ayhc <ayhcheung@gmail.com>
2023-04-07 13:26:19 -0400

    Add

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-# remove the default firefox (from fedora) in favor of the flatpak
-rpm-ostree override remove firefox firefox-langpacks
+
+echo "-- Installing RPMs from RPMFusion --"
+# Add packages that are not currently in ublue-os/main/packages.json
+rpm-ostree install \
+    mesa-vdpau-drivers-freeworld \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-bad-freeworld \
+    gstreamer1-plugins-ugly \
+    gstreamer1-vaapi \
+    compat-ffmpeg4 \
+    dptfxtract \
+    libavcodec-freeworld \
+    zerotier-one && \
+echo "---"
 
 echo "-- Installing RPMs defined in recipe.yml --"
 rpm_packages=$(yq '.rpms[]' < /tmp/ublue-recipe.yml)
@@ -10,10 +23,10 @@ for pkg in $(echo -e "$rpm_packages"); do \
 done
 echo "---"
 
-# install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
+# Install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
 pip install --prefix=/usr yafti
 
-# add a package group for yafti using the packages defined in recipe.yml
+# Add a package group for yafti using the packages defined in recipe.yml
 yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /etc/yafti.yml
 yq -i '.screens.applications.values.groups.Custom.default = true' /etc/yafti.yml
 flatpaks=$(yq '.flatpaks[]' < /tmp/ublue-recipe.yml)
